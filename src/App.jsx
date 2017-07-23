0254bf676520341513b58c06305d0b816dc0dbdf
Silpa Aleti <SilpaAleti@apples-MBP.home>
2017-07-23 18:36:26 -0400

    transform and bundle

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import IssueList from './IssueList.jsx';
 
+
 const contentNode = document.getElementById('contents');
-ReactDOM.render(<IssueList />, contentNode); 
+ReactDOM.render(<IssueList />, contentNode);
